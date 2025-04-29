6b78e5ff17d5881d5bb493f04cbc8c36d35bc49e
Bolaji87 <sakabolaji77@gmail.com>
2025-04-29 13:06:18 +0100

    install supabase and react-query

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://dzmkpmpvifflpjawpjms.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6bWtwbXB2aWZmbHBqYXdwam1zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU4Nzg5ODAsImV4cCI6MjA2MTQ1NDk4MH0.YrfK9oCoFENQ7_pnVqMB7jWwhBB_XEq4I98yRJId1gU";
+const supabase = createClient(supabaseUrl, supabaseKey);
+export default supabase;
