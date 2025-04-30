952ec88afbd14d2349ae1b9476a4202360addb55
Bolaji87 <sakabolaji77@gmail.com>
2025-04-30 23:26:37 +0100

    install and use react hot toast to manage nofitification for better ux and install react hook form to manage form

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,17 +1,29 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
+import { useState } from "react";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
+
+  function handleShowForm() {
+    setShowForm((prev) => !prev);
+  }
+
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-        <p>Filter/ Sort</p>
+        <p>Filter / Sort</p>
       </Row>
 
       <Row>
         <CabinTable />
+        <Button onClick={handleShowForm}>Add new cabin</Button>
+
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
