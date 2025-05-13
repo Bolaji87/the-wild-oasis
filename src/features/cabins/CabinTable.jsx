07300f177af0030ec9bcd6bf2a28a3952530aa54
Bolaji87 <sakabolaji77@gmail.com>
2025-05-13 22:48:36 +0100

    handle cabin  filtering and sorting using query string parameter

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,4 +1,3 @@
-import styled from "styled-components";
 import React from "react";
 
 import Spinner from "../../ui/Spinner";
@@ -6,21 +5,7 @@ import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
-
-const TableHeader = styled.header`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-
-  background-color: var(--color-grey-50);
-  border-bottom: 1px solid var(--color-grey-100);
-  text-transform: uppercase;
-  letter-spacing: 0.4px;
-  font-weight: 600;
-  color: var(--color-grey-600);
-  padding: 1.6rem 2.4rem;
-`;
+import { useSearchParams } from "react-router-dom";
 
 function CabinTable() {
   // before custom hook ⬇️
@@ -32,9 +17,33 @@ function CabinTable() {
   //   queryKey: ["cabins"],
   //   queryFn: getCabins,
   // });
-  const [isLoading, cabins, error] = useCabins();
+  const { isLoading, cabins } = useCabins();
 
+  const [searchParams] = useSearchParams();
   if (isLoading) return <Spinner />;
+
+  //1) FILTER
+  const filteredValue = searchParams.get("discount") || "all";
+
+  let filteredCabins;
+  if (filteredValue === "all") filteredCabins = cabins;
+
+  if (filteredValue === "no-discount")
+    filteredCabins = cabins.filter?.((cabin) => cabin.discount === 0);
+
+  if (filteredValue === "with-discount")
+    filteredCabins = cabins.filter?.((cabin) => cabin.discount > 0);
+
+  //2) SORT
+
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
+
   return (
     <Menus>
       <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
@@ -48,7 +57,9 @@ function CabinTable() {
         </Table.Header>
 
         <Table.Body
-          data={cabins}
+          // data={cabins}
+          // data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow key={cabin.id} cabin={cabin} />}
         />
       </Table>
--- src/features/cabins/useCabins.js
+++ src/features/cabins/useCabins.js
@@ -11,5 +11,5 @@ export function useCabins() {
     queryFn: getCabins,
   });
 
-  return [isLoading, cabins, error];
+  return { isLoading, cabins, error };
 }
